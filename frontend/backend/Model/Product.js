34eebdeb1ab1e438047e3396b6a96c2383e11c86
Faruq-Feroz <164517766+Faruq-Feroz@users.noreply.github.com>
2025-03-01 07:43:32 +0300

    Delete frontend/backend directory

--- frontend/backend/Model/Product.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require('mongoose');
-const productSchema = new mongoose.Schema({
-  brand: String,
-  name: String,
-  price: Number,
-  img: String,
-});
-module.exports=mongoose.model("Product",productSchema)
--- frontend/backend/routes/productRoute.js
+++
@@ -1,7 +0,0 @@
-const express=require("express");
-const {getProduct}=require("../controllers/productController");
-
-const router=express.Router();
-
-router.get("/",getProduct);
-module.exports=router;
